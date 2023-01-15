88af008c8744a8a930795e8e4e83838e505749d9
Oleksandr SIDLETSKYI <oleksandr.sidletskyi@smile-ukraine.com>
2023-01-15 15:19:33 +0200

    Try to change some styles

--- src/components/Training/TrainingPrograms/ProgramExpand/ProgramExpandInfo/ProgramExpandExercises/ProgramDay/DayExercise/DayExercise.tsx
+++ src/components/Training/TrainingPrograms/ProgramExpand/ProgramExpandInfo/ProgramExpandExercises/ProgramDay/DayExercise/DayExercise.tsx
@@ -4,10 +4,9 @@ import {DayExerciseProps} from "./DayExercise.types";
 
 const DayExercise:React.FC<DayExerciseProps> = ({exercise}) => {
     return (
-        <div>
-            <span>{exercise.id}: </span>
-            <span>{exercise.name}</span>
-        </div>
+        <p>
+          {exercise.id}: {exercise.name}
+        </p>
     )
 }
 
