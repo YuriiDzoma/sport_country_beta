7cf168aa99640cea100216f10037eb65de55ba2b
Yurii Dzoma <yuriidzoma@gmail.com>
2023-02-09 16:30:20 +0200

    ref host

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -3,7 +3,7 @@ import styles from './Users.module.scss'
 const Users = () => {
     return (
         <div className={styles.users}>
-            <span>Page not ready</span>
+            <span>Sorry, page not ready</span>
         </div>
     )
 }
