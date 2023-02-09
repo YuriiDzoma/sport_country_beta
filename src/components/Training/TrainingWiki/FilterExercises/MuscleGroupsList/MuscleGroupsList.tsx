d8b812593c78cf3882350a03029a54e1e6cd037a
Yurii Dzoma <yuriidzoma@gmail.com>
2023-02-09 19:05:11 +0200

    styling links

--- src/components/Training/TrainingWiki/FilterExercises/MuscleGroupsList/MuscleGroupsList.tsx
+++ src/components/Training/TrainingWiki/FilterExercises/MuscleGroupsList/MuscleGroupsList.tsx
@@ -1,21 +1,26 @@
 import {useAppDispatch, useAppSelector} from "hooks/redux";
 import { getIsFetching, getMuscleGroups } from "store/selectors";
 import Preloader from "components/Common/Preloader/Preloader";
-import React from "react";
+import React, {useState} from "react";
 import styles from './MuscleGroupsList.module.scss'
 import {setExercises} from "store/actions";
+import {muscleGroup} from "store/wikiExercises-slyce.types";
 
 const MuscleGroupsList = () => {
+    const [activeLink, setActiveLink] = useState('');
+
+    const onChangeGroup = (item: muscleGroup) => {
+        dispatch(setExercises(item.exercises));
+        setActiveLink(item.id)
+    }
     const dispatch = useAppDispatch();
     const muscleGroups = useAppSelector(getMuscleGroups);
     const isFetching = useAppSelector(getIsFetching);
     return (
         <div className={styles.groupsLinks}>
-            {isFetching ? <Preloader /> : muscleGroups.map((item, index) => (
-                <div key={index}>
-                    <span onClick={() => dispatch(setExercises(item.exercises))} >{item.title}</span>
-                </div>
-            ))}
+            {isFetching ? <Preloader /> : muscleGroups.map((item, index) =>
+                <a className={activeLink === item.id ? styles.activeLink : '' }
+                   key={index} onClick={() => onChangeGroup(item) } >{item.title}</a>)}
         </div>
     )
 }
