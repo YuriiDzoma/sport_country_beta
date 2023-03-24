c2eaeb11747263a36681e73e975ba45e58d96a10
Yurii Dzoma <yuriidzoma@gmail.com>
2023-03-24 11:04:14 +0200

    ref back

--- src/components/Friends/Friends.tsx
+++ src/components/Friends/Friends.tsx
@@ -1,14 +1,12 @@
 import {useParams} from "react-router";
 import {useAppDispatch, useAppSelector} from "hooks/redux";
 import {getIsFetchingUsers, getUserById} from "store/selectors";
-import styles from './Friends.module.scss'
 import React from "react";
 import UserInfo from "components/Common/UserInfo/UserInfo";
 import FriendList from "components/Friends/FriendsList/FriendList";
 import Preloader from "components/Common/Preloader/Preloader";
 import {getUserFriends} from "api/api";
 import {setFollowers} from "store/users-slice";
-import GoBack from "components/Common/GoBack/GoBack";
 
 const Friends = () => {
     const {id} = useParams();
@@ -20,10 +18,7 @@ const Friends = () => {
 
     const profile = useAppSelector((state) => getUserById(state, id));
     return (
-        <div className={styles.friendWrapper}>
-            <div className={styles.friendWrapper__goBack}>
-                <GoBack />
-            </div>
+        <div>
             <UserInfo id={id} profile={profile} />
             {isLoading ? <Preloader /> : <FriendList />}
         </div>
