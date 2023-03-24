50912bba7ebc59690ff0318d74b8c512e1d53cc8
Yurii Dzoma <yuriidzoma@gmail.com>
2023-03-24 11:39:29 +0200

    connect button go back

--- src/components/Common/UserInfo/UserInfo.tsx
+++ src/components/Common/UserInfo/UserInfo.tsx
@@ -3,12 +3,16 @@ import emptyProfileImage from "assets/img/emptyprofile.jpg";
 import React from "react";
 import {UserInfoProps} from "components/Common/UserInfo/UserInfo.types";
 import { Link } from 'react-router-dom';
+import GoBack from "components/Common/GoBack/GoBack";
 
 const UserInfo = ({ id, profile }:UserInfoProps) => {
     return (
         <>
             {profile && id && (
                 <div className={styles.userInfo}>
+                    <div className={styles.userInfo__goBack}>
+                        <GoBack />
+                    </div>
                     <Link className={styles.userInfo__link} to={'/profile/' + id}>
                         <img alt='user' className={styles.userInfo__photo}
                              src={profile.photoURL ? profile.photoURL : emptyProfileImage}>
