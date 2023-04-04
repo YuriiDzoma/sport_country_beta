429410ee62811e94cbd0c6a18cdc3a98db5e2ae8
Oleksandr SIDLETSKYI <oleksandr.sidletskyi@smile-ukraine.com>
2023-04-04 10:27:12 +0300

    Profile page styling

--- src/components/Common/FullPicture/FullPicture.tsx
+++ src/components/Common/FullPicture/FullPicture.tsx
@@ -10,13 +10,11 @@ const FullPicture = ({picture, setShowFullImage}: FullPicturesProps) => {
     return (
         <div className={styles.wrapper}>
             <div className={styles.wrapper__background} onClick={() => setShowFullImage(false)}></div>
-            <div className={styles.imageBox} >
-                <img className={styles.imageBox__image} src={picture} alt={'image'} />
-                <button  className={styles.imageBox__close}>
-                    <CloseIcon color={'warning'}
-                               onClick={() => setShowFullImage(false)} fontSize={'large'} />
-                </button>
-            </div>
+              <img className={styles.imageBox__image} src={picture} alt={'image'} />
+              <button  className={styles.imageBox__close}>
+                  <CloseIcon color={'warning'}
+                             onClick={() => setShowFullImage(false)} fontSize={'large'} />
+              </button>
         </div>
     )
 }
